e3a166b2d0ef6cb0392da32feccb85f0b8dae879
tP-Dms <osypl20085@naver.com>
2024-08-21 16:29:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // script.js
 
 document.getElementById('envelope').addEventListener('click', function() {
-    const invitation = document.querySelector('.invitation');
-    invitation.classList.add('flip');
+    this.classList.toggle('flip');
 });

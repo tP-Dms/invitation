2749e7930f5bbe6019582cc5f63302c00d2392ab
tP-Dms <osypl20085@naver.com>
2024-08-21 09:01:06 +0900

    Add files via upload

---
+++ project/script.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.getElementById('envelope').addEventListener('click', function() {
+    const invitation = document.querySelector('.invitation');
+    invitation.classList.add('flip');
+});
